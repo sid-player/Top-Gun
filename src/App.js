0f1134d065fc7a40155a47a29f9bf62296c49e2c
subhradeep1007 <78738221+subhradeep1007@users.noreply.github.com>
2021-06-10 19:36:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,11 +9,10 @@ import Signup from "./Container/SignupContainer";
 import Home from "./Container/HomeContainer";
 import Loading from "./Container/LoadingScreen";
 import Bookmark from "./Container/BookmarksContainer";
-<<<<<<< Updated upstream
-=======
+
 import Portfolio from "./Container/PortfolioContainer"
 import Team from "./Container/TeamContainer"
->>>>>>> Stashed changes
+
 
 export default function App() {  
 
@@ -27,12 +26,11 @@ export default function App() {
         <Route exact path="/Signup" component={Signup} />
         <Route exact path="/Loading" component={Loading} />
         <Route exact path="/Bookmarks" component={Bookmark}/>
-<<<<<<< Updated upstream
-=======
+
         <Route exact path="/Portfolio" component={Portfolio}/>
         <Route exact path="/Team" component={Team}/>
 
->>>>>>> Stashed changes
+
       </Switch>
     </Router>
   );
