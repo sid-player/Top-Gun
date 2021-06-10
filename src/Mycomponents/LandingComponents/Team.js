0593e02ee9e6580a635987f701372b637eeb7d6e
Tanishjaingit10 <tanish.jain.961@gmail.com>
2021-06-10 23:55:03 +0530

    warnings

--- src/Mycomponents/LandingComponents/Team.js
+++ src/Mycomponents/LandingComponents/Team.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import logo4 from './Logo4.png'
+// import logo4 from './Logo4.png'
 import logo6 from './Logo6.png'
 
 const Team = () => {
-    let style = {
-        background: "linear-gradient(90deg, #d53369 0%, #daae51 100%)",
-    };
+    // let style = {
+    //     background: "linear-gradient(90deg, #d53369 0%, #daae51 100%)",
+    // };
     
     return (
         <section className="items-center justify-center h-screen w-screen pt-12">
@@ -21,7 +21,7 @@ const Team = () => {
                 <div className="lg:flex md:flex xl:justify-between flex-wrap md:justify-around sm:justify-around lg:justify-around pt-4">
                     <div className="xl:w-1/3 sm:w-1/2 mx-auto sm:max-w-xs mb-20 xl:max-w-sm lg:w-1/2 relative">
                         <div className="bg-top bg-cover bg-no-repeat h-68 w-68">
-                            <img src={logo6} alt className="h-full w-full overflow-hidden object-cover rounded shadow" />
+                            <img src={logo6} alt="img" className="h-full w-full overflow-hidden object-cover rounded shadow" />
                         </div>
                         
                         <div className="py-5 bg-white flex flex-col justify-center w-11/12 mx-auto absolute rounded shadow -mt-16 right-0 left-0">
@@ -33,7 +33,7 @@ Dharamveer Singh Chouhan
                     </div>
                     <div className="xl:w-1/3 sm:w-1/2 mx-auto sm:max-w-xs  mb-20 xl:max-w-sm lg:w-1/2 relative">
                         <div className="bg-top bg-cover bg-no-repeat h-68">
-                            <img src={logo6} alt className="h-full w-full overflow-hidden object-cover rounded shadow" />
+                            <img src={logo6} alt="img" className="h-full w-full overflow-hidden object-cover rounded shadow" />
                         </div>
                         <div className="py-5 bg-white flex flex-col justify-center w-11/12 mx-auto absolute rounded shadow -mt-12 right-0 left-0">
                             <p className="text-xl text-center text-gray-800 font-normal mb-1">Parth Choudhary</p>
