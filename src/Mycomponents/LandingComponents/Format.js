210d79dc314df0e9f3dc1c66bfce924580e842be
Sparsh Kandpal <sparsh@AKARSH.localdomain>
2021-06-10 15:48:04 +0530

    styling search

--- src/Mycomponents/LandingComponents/Format.js
+++ src/Mycomponents/LandingComponents/Format.js
@@ -3,7 +3,11 @@ import React, { useState } from 'react'
 function Url({object}){
     return (
         <>
-            <a href={object.url} rel="noreferrer" target="_blank" className="text-blue-800 pl-8 block text-left overflow-ellipsis">{object.name}</a>
+            <a href={object.url} rel="noreferrer" target="_blank" className=" pl-8 block text-left overflow-ellipsis">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+</svg>
+                 {object.name}</a>
         </>
     );
 }
@@ -16,7 +20,7 @@ function Format({object}) {
     {
         const formatted = object.children.map((obj)=><Format key={obj.id} object={obj}/>);
         return (
-            <div className="pl-8">
+            <div className="ml-8 pl-4">
                 <button onClick={(e)=>setVisbility((prev)=>!prev)} className="font-bold w-full text-left">{object.name}</button>
                 <div className={visibility?"":"hidden"}>{formatted}</div>
             </div>
@@ -26,3 +30,6 @@ function Format({object}) {
 }
 
 export default Format;
+
+
+
