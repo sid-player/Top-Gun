dde680cb5bd9f5d01363885ce7c9c9d97dca2771
SakshiVijay24 <70104243+SakshiVijay24@users.noreply.github.com>
2021-06-12 18:20:10 +0530

    About Us

--- src/Container/AboutContainer.js
+++ src/Container/AboutContainer.js
@@ -2,12 +2,15 @@ import React from 'react'
 
 import Header from "../Mycomponents/LandingComponents/Header";
 import About from "../Mycomponents/LandingComponents/About";
+import Content from '../Mycomponents/AboutComponents/Content';
+import Footer from '../Mycomponents/HomeComponents/Footer';
 
 function AboutConatiner() {
     return (
         <>
             <Header />
-            <About />
+            <Content />
+            <Footer />
         </>
     )
 }
