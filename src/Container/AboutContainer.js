cea0ccfe4a878bac074f357814e5bd9bc0b09a6f
SakshiVijay24 <70104243+SakshiVijay24@users.noreply.github.com>
2021-06-20 18:12:51 +0530

    About us in gray

--- src/Container/AboutContainer.js
+++ src/Container/AboutContainer.js
@@ -2,14 +2,12 @@ import React from 'react'
 
 import Header from "../Mycomponents/LandingComponents/Header";
 import Content from '../Mycomponents/AboutComponents/Content';
-import Footer from '../Mycomponents/HomeComponents/Footer';
 
 function AboutConatiner() {
     return (
         <>
             <Header />
             <Content />
-            <Footer />
         </>
     )
 }
--- src/Container/LoadingScreen.js
+++ src/Container/LoadingScreen.js
@@ -2,10 +2,6 @@ import React from 'react'
 
 function LoadingScreen() {
 
-    let style = {
-        background: "linear-gradient(90deg, #d53369 0%, #daae51 100%)",
-    };
-
     const loading = {
         borderWidth: "2px",
         borderRightColor: "transparent",
@@ -14,8 +10,8 @@ function LoadingScreen() {
     }
     
     return (
-        <div className="h-screen w-screen flex items-center justify-center" style={style}>
-            <p className="text-white">Loading </p>
+        <div className="h-screen w-screen flex items-center justify-center bg-gray-100">
+            <p className="text-gray-800">Loading </p>
             <div style={loading} className="h-4 w-4 animate-spin m-2 rounded-2xl border-solid"></div>
         </div>
     )
