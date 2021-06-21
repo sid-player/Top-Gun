0b85b2a3f5d7d5f3614d9bc49eb5d361140b168f
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-06-21 12:21:24 +0530

    Footer in teams page

--- src/Container/TeamContainer.js
+++ src/Container/TeamContainer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Mycomponents/LandingComponents/Header";
 import Team from "../Mycomponents/LandingComponents/Team";
+import Footer from "../Mycomponents/HomeComponents/Footer";
 
 const PortfolioContainer = () => {
   return (
     <div>
       <Header />
       <Team />
+      <Footer />
     </div>
   );
 };
