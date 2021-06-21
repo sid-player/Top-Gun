e6394f6945c36bf90d17b944ed4ca0eee80d7c36
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-06-21 13:00:02 +0530

    impr home page

--- src/Container/LandingContainer.js
+++ src/Container/LandingContainer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 import Header from "../Mycomponents/LandingComponents/Header";
 // import Footer from "../Mycomponents/LandingComponents/Footer";
@@ -6,33 +6,23 @@ import Hero from "../Mycomponents/LandingComponents/Hero";
 // import Title from "../Mycomponents/LandingComponents/Title";
 import Pricing from "../Mycomponents/LandingComponents/Pricing";
 import Team from "../Mycomponents/LandingComponents/Team";
-import Portfolio from "../Mycomponents/LandingComponents/Portfolio"
-// import Fade from 'react-reveal/Fade';
-import Slide from 'react-reveal/Slide';
-
+import Portfolio from "../Mycomponents/LandingComponents/Portfolio";
+import Fade from "react-reveal/Fade";
 
 function LandingContainer() {
-    return (
-        <>  
-            <div className="overflow-hidden">
-            <Header />
-            <Hero />
-            <Slide left>
-            <Team/>
-            {/* <div className='md:hidden mt- pt-'>
-            </div> */}
-            <Portfolio/>
-            
-            </Slide>
-            <Pricing />
-           
-            {/*
-            <Title />
-            <Footer /> */}
-            </div>
-            
-        </>
-    )
+  return (
+    <>
+      <div className="overflow-hidden">
+        <Header />
+        <Hero />
+        <Fade>
+          <Team />
+          <Portfolio />
+        </Fade>
+        <Pricing />
+      </div>
+    </>
+  );
 }
 
-export default LandingContainer
+export default LandingContainer;
--- src/Mycomponents/LandingComponents/Hero.js
+++ src/Mycomponents/LandingComponents/Hero.js
@@ -2,29 +2,26 @@ import React from "react";
 import logo from "./hero.png";
 
 export default function Hero() {
-  
-  
-  
   return (
     <div className="bg-gray-100">
-      <div className="pt-24 ">
-        <div className=" container   flex flex-wrap flex-col md:flex-row items-center bg-gray-100  p-6" >
-          <div className=" flex flex-col w-full md:w-1/2 justify-center items-start text-center md:text-left ">
-            <p className="uppercase tracking-loose text-gray-700 w-full">
-            Hi budding entrepreneurs!
-            </p>
-            <h1 className="my-4 text-5xl font-thin leading-tight text-gray-700">
-            Are you still trying to figure out the right tactics to grow your startup? 
+      <div className="pt-24">
+        <div className="flex flex-wrap flex-col md:flex-row items-center justify-center bg-gray-100 p-6">
+          <div className="flex flex-col w-full md:w-1/2 justify-center items-center text-center md:text-left">
+            <h1
+              className="my-4 text-4xl sm:text-5xl text-gray-700 leading-tight md:leading-none"
+              style={{ lineHeight: "3.5rem" }}
+            >
+              Trying to figure out the right tactics for your startup?
             </h1>
-            <p className="leading-normal   text-2xl mb-8 text-gray-700">
-            If yes, then you are at the right place.
-            </p>
-            <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out ">
-              Subscribe
+            <h1 className="text-xl sm:text-3xl mb-8 text-gray-700 font-thin self-center md:self-start">
+              Then you are at the right place.
+            </h1>
+            <button className="mx-auto lg:mx-0 hover:bg-gray-500 hover:text-white bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out self-center sm:self-start">
+              Signup Now!
             </button>
           </div>
           <div className="w-full md:w-1/2 py-6 text-center">
-            <img className="w-full md:w-4/5 z-50" src={logo} alt="img"/>
+            <img className="w-full md:w-4/5 z-50 " src={logo} alt="img" />
           </div>
         </div>
       </div>
