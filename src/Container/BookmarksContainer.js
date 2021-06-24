8f75b0e33745523c7fa039b64286637086ebb616
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-06-24 18:05:26 +0530

    improved serachbar

--- src/Container/BookmarksContainer.js
+++ src/Container/BookmarksContainer.js
@@ -1,21 +1,18 @@
-import React from 'react'
+import React from "react";
 // import data from '.././BookmarkData';
 import Header from "../Mycomponents/LandingComponents/Header";
 import Search from "../Mycomponents/LandingComponents/SearchBar";
 
 function BookmarksContainer() {
-
-   
-    
-    return (
-        <>
-            <Header/>
-            <div className="pt-20 min-h-screen bg-gray-300">
-                <Search />
-                <div style={{height : "65vh"}}></div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div className="pt-20 min-h-screen bg-gray-100">
+        <Search />
+        <div style={{ height: "65vh" }}></div>
+      </div>
+    </>
+  );
 }
 
 export default BookmarksContainer;
--- src/Mycomponents/LandingComponents/SearchBar.js
+++ src/Mycomponents/LandingComponents/SearchBar.js
@@ -1,91 +1,106 @@
 import data from "../../BookmarkData";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import searchIcon from "../../assets/search.svg";
 
-function Search()
-{
-    const [SearchResults, setSearchResults] = useState(data)
-    const [listToDisplay, setListToDisplay] = useState(null)
+function Search() {
+  const [SearchResults, setSearchResults] = useState(data);
+  const [listToDisplay, setListToDisplay] = useState(null);
 
-    useEffect(()=>{
-        setListToDisplay(SearchResults.map( object => 
-        // <div className= "grid grid-flow-col grid-cols-3">
-    //    iterating over data
-       <div>
-            <a href={object.url} target="_blank" rel="noreferrer" className=" mt-3  justify-center text-sm md:text-l text-gray-700">
-                <div className=" mt-4 justify-center overflow-hidden shadow transform hover:scale-105 hover:mb-2 hover:bg-gray-100  bg-gray-50   break-words   border-b-2 rounded p-4 md:p-8 my-1 font-medium  "
-              key={object.id}>
-                {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-4 mr-3 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
-                </svg> */}
-                {/* for dummy image */}
-                {
-                    object.image.length !==0 ?
-                   <img src={object.image} alt="" className="w-full h-40 object-contain  justify-center"/>
-                    :
-                   <img src="https://picsum.photos/id/1/180" alt="" className="h-40
-                    object-contain justify-center"/>
-                }
+  useEffect(() => {
+    setListToDisplay(
+      SearchResults.map((object) => (
+        <div>
+          <a
+            href={object.url}
+            target="_blank"
+            rel="noreferrer"
+            className=" mt-3  justify-center text-sm md:text-l text-gray-700"
+          >
+            <div
+              className=" mt-4 justify-center overflow-hidden shadow transform hover:scale-105 hover:mb-2 hover:bg-gray-100  bg-gray-50   break-words   border-b-2 rounded p-4 md:p-8 my-1 font-medium  "
+              key={object.id}
+            >
+              {/* for dummy image */}
+              {object.image.length !== 0 ? (
+                <img
+                  src={object.image}
+                  alt=""
+                  className="w-full h-40 object-contain  justify-center"
+                />
+              ) : (
+                <img
+                  src="https://picsum.photos/id/1/180"
+                  alt=""
+                  className="h-40
+                    object-contain justify-center"
+                />
+              )}
 
-               <div className= " truncate">
-               {object.name}
-               </div>
-               
-                </div>
-            </a>
+              <div className=" truncate">{object.name}</div>
+            </div>
+          </a>
         </div>
-        ))
-    }, [SearchResults])
-    
-    function onChangeHandler(value)
-    {
-        if(value===""){
-            setSearchResults(data);
-            return;
-        }
-        const searchArray = value.toLowerCase().split(" ").filter(word=>word.length>0);
-        let scoreArray = []
-        data.forEach( object => {
-            let score=0;
-            let nameArray = object.name.toLowerCase().split(" ").filter(word=>word.length>0)
-            searchArray.forEach( word => {
-                if(object.name.toLowerCase().includes(word))score+=5;
-                if(object.url.toLowerCase().includes(word))score++;
-                if(nameArray.includes(word))score+=5;
-            });
-            if(score)scoreArray.push([score,object]);
-        })
-        if(scoreArray.length){
-            scoreArray.sort((a,b)=>b[0]-a[0]);
-            let tempSearchResults = []
-            scoreArray.forEach( pair => tempSearchResults.push( pair[1] ));
-            setSearchResults( tempSearchResults )
-        }
-        else{
-            setListToDisplay(<div className="bg-white rounded p-5 my-5  ">No Match Found</div>);
-        }
-    }
-    
-    return (
-
-        // search bar
-    <div className="Search my-4 w-screen flex flex-col items-center justify-center">
-        <div className=" border-2 border-gray-200 shadow-md w-4/5 md:w-2/3 relative  bg-gray-100 rounded-lg flex">
-            <input className="w-full rounded-full bg-gray-100 p-4  h-16 placeholder-gray-900" type="text" placeholder="Search Anything..." onChange={e=>onChangeHandler(e.target.value)}/>
-             
-        </div>
-
-        
-        
-        
-
-        {/* content */}
-           
-        <div className="relative mt-0  overflow-hidden grid gap-6 sm:grid-cols-1 w-1/2 h-1/2  md:w-2/3 grid-cols-3 lg:grid-cols-4">
-            {listToDisplay}
-        </div>
-
-    </div>
+      ))
     );
+  }, [SearchResults]);
+
+  function onChangeHandler(value) {
+    if (value === "") {
+      setSearchResults(data);
+      return;
+    }
+    const searchArray = value
+      .toLowerCase()
+      .split(" ")
+      .filter((word) => word.length > 0);
+    let scoreArray = [];
+    data.forEach((object) => {
+      let score = 0;
+      let nameArray = object.name
+        .toLowerCase()
+        .split(" ")
+        .filter((word) => word.length > 0);
+      searchArray.forEach((word) => {
+        if (object.name.toLowerCase().includes(word)) score += 5;
+        if (object.url.toLowerCase().includes(word)) score++;
+        if (nameArray.includes(word)) score += 5;
+      });
+      if (score) scoreArray.push([score, object]);
+    });
+    if (scoreArray.length) {
+      scoreArray.sort((a, b) => b[0] - a[0]);
+      let tempSearchResults = [];
+      scoreArray.forEach((pair) => tempSearchResults.push(pair[1]));
+      setSearchResults(tempSearchResults);
+    } else {
+      setListToDisplay(
+        <div className="bg-white rounded p-5 my-5  ">No Match Found</div>
+      );
+    }
+  }
+
+  return (
+    // search bar
+    <div className="Search my-4 w-screen flex flex-col items-center justify-center">
+      <div className="border-1 border-gray-100 shadow w-4/5 md:w-2/3 relative flex my-2">
+        <span className="flex items-center justify-center bg-white pl-4 z-20">
+          <img src={searchIcon} alt="search icon" style={{ height: "2vh" }} />
+        </span>
+        <input
+          className="bg-white appearance-none border-2 border-white  w-full py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-md z-0"
+          type="text"
+          placeholder="Search Anything..."
+          onChange={(e) => onChangeHandler(e.target.value)}
+        />
+      </div>
+
+      {/* content */}
+
+      <div className="relative mt-0  overflow-hidden grid gap-6 sm:grid-cols-1 w-1/2 h-1/2  md:w-2/3 grid-cols-3 lg:grid-cols-4">
+        {listToDisplay}
+      </div>
+    </div>
+  );
 }
 
 export default Search;
