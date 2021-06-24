dac34d03a8a7dd8c68513c5a3e346f09dae43126
mukulsoni-op <78497619+mukulsoni-op@users.noreply.github.com>
2021-06-24 23:44:02 +0530

    material ui
    installed material ui

--- src/Mycomponents/LandingComponents/SearchBar.js
+++ src/Mycomponents/LandingComponents/SearchBar.js
@@ -1,5 +1,6 @@
 import data from "../../BookmarkData";
 import {useState, useEffect} from "react";
+import SearchIcon from '@material-ui/icons/Search';
 
 function Search()
 {
@@ -17,6 +18,7 @@ function Search()
                 {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-4 mr-3 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
                 </svg> */}
+
                 {/* for dummy image */}
                 {
                     object.image.length !==0 ?
@@ -69,8 +71,10 @@ function Search()
 
         // search bar
     <div className="Search my-4 w-screen flex flex-col items-center justify-center">
+
         <div className=" border-2 border-gray-200 shadow-md w-4/5 md:w-2/3 relative  bg-gray-100 rounded-lg flex">
-            <input className="w-full rounded-full bg-gray-100 p-4  h-16 placeholder-gray-900" type="text" placeholder="Search Anything..." onChange={e=>onChangeHandler(e.target.value)}/>
+        {/* <SearchIcon /> */}
+        <input className="w-full rounded-full bg-gray-100 p-4  h-16 placeholder-gray-900" type="text" placeholder="Search Anything..." onChange={e=>onChangeHandler(e.target.value)}/>
              
         </div>
 
