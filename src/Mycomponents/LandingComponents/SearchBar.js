8fda9f2ab53441543647fa7c966a6d19c1bcfc1b
SakshiVijay24 <70104243+SakshiVijay24@users.noreply.github.com>
2021-06-28 00:08:34 +0530

    Update SearchBar.js

--- src/Mycomponents/LandingComponents/SearchBar.js
+++ src/Mycomponents/LandingComponents/SearchBar.js
@@ -15,7 +15,8 @@
 //     //    iterating over data
 //        <div>
 //             <a href={object.url} target="_blank" rel="noreferrer" className=" mt-3  justify-center text-sm md:text-l text-gray-700">
-//                 <div className=" mt-4 justify-center overflow-hidden shadow transform hover:scale-105 hover:mb-2 hover:bg-gray-100  bg-gray-50   break-words   border-b-2 rounded p-4 md:p-8 my-1 font-medium  "
+//                 <div className=" mt-4 justify-center overflow-hidden shadow transform hover:scale-105 hover:mb-2 hover:bg-gray-100  bg-gray-50  
+//                   break-words   border-b-2 rounded p-4 md:p-8 my-1 font-medium  "
 //               key={object.id}>
                
 //                 {/* for dummy image */}
@@ -92,7 +93,8 @@
 
 //         <div className=" border-2 border-gray-200 shadow-md w-2/5 md:w-2/3 relative  bg-gray-100 rounded-lg flex">
 //         {/* <SearchIcon> */}
-//             <input className="w-full rounded-full bg-gray-100 p-4  h-16 placeholder-gray-900" type="text" placeholder="Search Anything..." onChange={exactSearch} value={Search} />
+//             <input className="w-full rounded-full bg-gray-100 p-4  h-16 placeholder-gray-900" type="text" placeholder="Search Anything..." 
+//              onChange={exactSearch} value={Search} />
 //              <button onClick= {onChangeHandler} className= "ml-4 rounded-lg " > Search </button>
 //         </div>
 
@@ -129,6 +131,8 @@ function Search()
     const [Search, setSearch]= useState("");
     const [SearchResults, setSearchResults] = useState(data)
     const [listToDisplay, setListToDisplay] = useState(null)
+   
+    
 
     useEffect(()=>{
         setListToDisplay(SearchResults.map( object => 
@@ -136,7 +140,8 @@ function Search()
     //    iterating over data
        <div>
             <a href={object.url} target="_blank" rel="noreferrer" className=" mt-3  justify-center text-sm md:text-l text-gray-700">
-                <div className=" mt-4 justify-center overflow-hidden shadow transform hover:scale-105 hover:mb-2 hover:bg-gray-100  bg-gray-50   break-words   border-b-2 rounded p-4 md:p-8 my-1 font-medium  "
+                <div className=" mt-4 justify-center overflow-hidden shadow transform hover:scale-105 hover:mb-2 hover:bg-gray-100  
+                bg-gray-50 break-words border-b-2 rounded p-6 md:p-8 my-1 font-medium  "
               key={object.id}>
                
                 {/* for dummy image */}
@@ -152,8 +157,8 @@ function Search()
                {object.name}
                
                </div>
-               <div>
-                   Tags-{object.Tag}
+               <div className="border-2 rounded-xl text-center  mx-auto mt-2 bg-gray-200 font-bold text-gray-900">
+                   {object.Tag}
                </div>
                
                 </div>
@@ -184,7 +189,7 @@ function Search()
         // },3000);
 
         // setloading(true);
-
+    
         const searchArray = value.toLowerCase().split(" ").filter(word=>word.length>0);
         let scoreArray = []
         data.forEach( object => {
@@ -209,25 +214,32 @@ function Search()
 
         // setloading(false);
     }
+
+    
     
     return (
 
         // search bar
-    <div className="Search my-4 w-screen flex flex-col items-center justify-center">
+    <div className="Search my-4 flex flex-col items-center justify-center">
 
-        <div className=" border-2 border-gray-200 shadow-md w-2/5 md:w-2/3 relative  bg-gray-100 rounded-lg flex">
+        <div className=" border-2 border-gray-200 shadow-sm md:shadow-md w-4/5 md:w-2/3 relative  bg-gray-100 rounded-lg flex">
         {/* <SearchIcon> */}
-            <input className="w-full rounded-full bg-gray-100 p-4  h-16 placeholder-gray-900" type="text" placeholder="Search Anything..." onChange={exactSearch} value={Search} />
-             <button onClick= {onChangeHandler} className= "ml-4 rounded-lg " > Search </button>
+            <input className="w-full rounded-full bg-gray-100 p-2  h-12  placeholder-gray-900" type="text" placeholder="Search Anything..." onChange={exactSearch} value={Search} />
+             <button onClick= {onChangeHandler} className= "m-1 rounded-lg bg-gray-700 px-4 text-gray-100 hover:bg-gray-900" > Search </button>
         </div>
 
-        
+        <div className='my-3 flex flex-wrap -m-1 w-3/4 md:w-2/3'>
+                <button className="m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose cursor-pointer" >Miscelleneous</button>
+                <button className="m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose cursor-pointer">Miscelleneous</button>
+                <button className="m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose cursor-pointer">Miscelleneous</button>
+            
+            </div>
         
         
 
         {/* content */}
            
-           <div className="relative mt-0  overflow-hidden grid  gap-6 sm:grid-cols-1 w-1/2 h-1/2  md:w-2/3 grid-cols-3 lg:grid-cols-4">
+           <div className="relative mt-0 overflow-hidden grid  gap-6 sm:grid-cols-1 w-3/4  md:w-2/3 grid-cols-2 lg:grid-cols-4">
            {
             //    loading === false ?
               
@@ -248,6 +260,7 @@ function Search()
 
     </div>
     );
+   
 }
 
 export default Search;
