d0caf1ee17438840ed6aac8662ec002c03cffb15
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-07-05 08:36:59 +0530

    design impr in about page

--- src/Mycomponents/AboutComponents/Content.js
+++ src/Mycomponents/AboutComponents/Content.js
@@ -1,36 +1,41 @@
-import React from 'react'
-
+import React from "react";
 
 function Content() {
-    return (
-        <div className="mt-12 md:mt-16 md:pt-4 flex flex-col items-center  h-screen bg-gray-100 ">
-            <h1 className="w-full mt-10 text-3xl md:text-5xl font-bold leading-tight text-center text-gray-800">
-                About Us
-            </h1>
-            <div className="bg-gray-100 hover:grow shadow-xl my-6 md:my-8 mx-3 lg:mx-auto w-full md:w-4/5 lg:w-3/5 text-md md:text-xl
-              text-gray-700 font-semibold text-left rounded-md px-8 py-6">
-                <p>
-                    TopGun.VC builds entrepreneur’s faith in their vision and nurtures them with direction for the long war ahead.
-                </p>
-                <p>
-                    We believe companies are an outcome of the vision, character and determination of their founders. We believe
-                    billion-dollar companies can be built in any market, in any geography over the next decade. So, it’s the entrepreneur
-                    who wants to make it that matters. And we love to work with the most brutal grinders out there, and there are few joys
-                    other than vicariously winning against odds.
-                </p>
-                <p className="mt-2">
-                    We are founders ourselves and work part-time to help as many entrepreneurs as possible on their mission forward.
-                    We love equity and make sure we are participating for the long game ahead. If you would like to work with us, let us know.
-                </p>
-                <div className="mt-10 outline">
-                    Thanks 
-                    <p className="font-bold underline">
-                        Dharamveer &amp; Parth
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="mt-12 md:mt-16 md:pt-4 flex flex-col items-center h-screen bg-gray-100 ">
+      <h1 className="w-full mt-10 text-3xl md:text-5xl font-bold leading-tight text-center text-gray-800">
+        About Us
+      </h1>
+      <div
+        className="shadow-xl my-6 md:my-8 mx-3 lg:mx-auto w-full md:w-4/5 lg:w-3/5 text-md md:text-xl
+              text-gray-700 text-center md:text-left rounded-md px-8 py-6 border-2"
+        style={{ lineHeight: "1.8em" }}
+      >
+        <p className="p-2">
+          TopGun.VC builds entrepreneur’s faith in their vision and nurtures
+          them with direction for the long war ahead.
+        </p>
+        <p className="p-2">
+          We believe companies are an outcome of the vision, character and
+          determination of their founders. We believe billion-dollar companies
+          can be built in any market, in any geography over the next decade. So,
+          it’s the entrepreneur who wants to make it that matters. And we love
+          to work with the most brutal grinders out there, and there are few
+          joys other than vicariously winning against odds.
+        </p>
+        <p className="p-2">
+          We are founders ourselves and work part-time to help as many
+          entrepreneurs as possible on their mission forward. We love equity and
+          make sure we are participating for the long game ahead. If you would
+          like to work with us, let us know.
+        </p>
+        <div className="mt-10 outline p-2">
+          Thanks
+          <p className="font-bold underline">Dharamveer &amp; Parth</p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Content
+export default Content;
