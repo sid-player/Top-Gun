6b2dc9d1267b5d387b3a185e31d758cb97e26a6b
subhradeep1007 <subhradeep1007@gmail.com>
2021-07-09 03:42:51 +0530

    Improved About and Cards Redesigned

--- src/Mycomponents/AboutComponents/Content.js
+++ src/Mycomponents/AboutComponents/Content.js
@@ -1,37 +1,237 @@
 import React from "react";
+import logo1 from "../LandingComponents/img/Logo1.png";
+import logo2 from "../LandingComponents/img/Logo2.jpg";
+import logo3 from "../LandingComponents/img/Logo3.png";
+import logo4 from "../LandingComponents/img/Logo4.png";
+import logo5 from "../LandingComponents/img/Logo5.jpg";
+import logo6 from "../LandingComponents/img/Logo6.png";
+import Marquee from "react-fast-marquee";
 
 function Content() {
   return (
-    <div className="mt-12 md:mt-16 md:pt-4 flex flex-col items-center h-full bg-gray-100 ">
-      <h1 className="w-full mt-6 text-3xl md:text-5xl font-bold leading-tight text-center text-gray-800">
-        About Us
-      </h1>
-      <div
-        className="shadow-xl my-6 md:my-8 mx-3 lg:mx-auto w-full md:w-4/5 lg:w-3/5 text-md md:text-xl
-              text-gray-700 text-center md:text-left rounded-md px-8 py-6 border-2"
-        style={{ lineHeight: "1.8em" }}
-      >
-        <p className="p-2">
-          TopGun.VC builds entrepreneur’s faith in their vision and nurtures
-          them with direction for the long war ahead.
-        </p>
-        <p className="p-2">
-          We believe companies are an outcome of the vision, character and
-          determination of their founders. We believe billion-dollar companies
-          can be built in any market, in any geography over the next decade. So,
-          it’s the entrepreneur who wants to make it that matters. And we love
-          to work with the most brutal grinders out there, and there are few
-          joys other than vicariously winning against odds.
-        </p>
-        <p className="p-2">
-          We are founders ourselves and work part-time to help as many
-          entrepreneurs as possible on their mission forward. We love equity and
-          make sure we are participating for the long game ahead. If you would
-          like to work with us, let us know.
-        </p>
-        <div className="mt-10 outline p-2">
-          Thanks
-          <p className="font-bold underline">Dharamveer &amp; Parth</p>
+    // <div className="mt-12 md:mt-16 md:pt-4 flex flex-col items-center h-full bg-gray-100 ">
+    //   <h1 className="w-full mt-6 text-3xl md:text-5xl font-bold leading-tight text-center text-gray-800">
+    //     About Us
+    //   </h1>
+
+    //   <div
+    //     className="shadow-xl my-6 md:my-8 mx-3 lg:mx-auto w-full md:w-4/5 lg:w-3/5 text-md md:text-xl
+    //           text-gray-700 text-center md:text-left rounded-md px-8 py-6 border-2"
+    //     style={{ lineHeight: "1.8em" }}
+    //   >
+    //     <p className="p-2">
+    //       TopGun.VC builds entrepreneur’s faith in their vision and nurtures
+    //       them with direction for the long war ahead.
+    //     </p>
+    //     <p className="p-2">
+    //       We believe companies are an outcome of the vision, character and
+    //       determination of their founders. We believe billion-dollar companies
+    //       can be built in any market, in any geography over the next decade. So,
+    //       it’s the entrepreneur who wants to make it that matters. And we love
+    //       to work with the most brutal grinders out there, and there are few
+    //       joys other than vicariously winning against odds.
+    //     </p>
+    //     <p className="p-2">
+    //       We are founders ourselves and work part-time to help as many
+    //       entrepreneurs as possible on their mission forward. We love equity and
+    //       make sure we are participating for the long game ahead. If you would
+    //       like to work with us, let us know.
+    //     </p>
+    //     <div className="mt-10 outline p-2">
+    //       Thanks
+    //       <p className="font-bold underline">Dharamveer &amp; Parth</p>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="pt-0 bg-white  overflow-hidden">
+      <div className=" container   flex flex-wrap flex-col md:flex-row  bg-white p-6 ">
+        <div className=" flex flex-col w-full md:w-1/2 items-start text-center md:text-left ">
+          <h1 className="w-full px-12 mt-6 md:mt-32 my-2 text-4xl font-medium text-left text-blue-1000">
+            Join Our Team
+          </h1>
+          <div className="border-b-4 mx-12 rounded-xl border-gray-700 w-12"></div>
+
+          <div className=" px-12  md:text-l " style={{ lineHeight: "1.8em" }}>
+            <p className="p-2 text-left">
+              TopGun.VC builds entrepreneur’s faith in their vision and nurtures
+              them with direction for the long war ahead.
+            </p>
+            <p className="p-2 text-left">
+              We believe companies are an outcome of the vision, character and
+              determination of their founders. We believe billion-dollar
+              companies can be built in any market, in any geography over the
+              next decade. So, it’s the entrepreneur who wants to make it that
+              matters. And we love to work with the most brutal grinders out
+              there, and there are few joys other than vicariously winning
+              against odds.
+            </p>
+            <p className="p-2 text-left">
+              We are founders ourselves and work part-time to help as many
+              entrepreneurs as possible on their mission forward. We love equity
+              and make sure we are participating for the long game ahead. If you
+              would like to work with us, let us know.
+            </p>
+            <div className="mt-4 text-left outline p-2">
+              Thanks
+              <p className="font-bold underline text-left">Dharamveer &amp; Parth</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 py-6 text-center">
+          <div className=" mt-10 md:mt-20 ">
+            <section className="bg-white  py-4 h-full ">
+              <div className="container  flex flex-wrap pt-4 pb-12 mr-6">
+                <h1 className=" m-auto text-center my-0 text-3xl font-medium text-gray-900 border-b-4 border-gray-600 rounded-sm w-auto inline-block">
+                  Our Partners
+                </h1>
+
+                <Marquee
+                  gradient={false}
+                  className=" hidden "
+                  direction="right"
+                  speed="30"
+                  gradientWidth="0"
+                >
+                  <div className="w-full   md:w-1/3 p-0 flex flex-col flex-grow flex-shrink">
+                    <div
+                      className="
+                  flex-1
+                  bg-white
+                  overflow-hidden
+                  mb-16
+                "
+                    >
+                      <div className="mt-16">
+                        <a href="https://www.hiration.com/job-search/free-resume-builder/?gclid=Cj0KCQjw8IaGBhCHARIsAGIRRYrUxG0mw9_fhr73O1o0m-AZZWw3P9KDtjBLxB7B8cXIJjPL2M9Tx2kaAuo6EALw_wcB">
+                          <img
+                            className=" h-16 w-72 md:w-44 md:h-10 m-auto align-baseline"
+                            src={logo1}
+                            alt="img"
+                          />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="w-full  md:w-1/3 p-2 flex flex-col flex-grow flex-shrink">
+                    <div
+                      className="
+                  flex-1
+                  bg-white
+                  mb-16
+                  overflow-hidden
+                "
+                    >
+                      <div className="md:mt-16">
+                        <a href="https://marwaricatalysts.com/">
+                          <img
+                            className=" w-44 h-12 m-auto align-baseline"
+                            src={logo2}
+                            alt="img"
+                          />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="w-full   md:w-1/3 p-2 flex flex-col flex-grow flex-shrink">
+                    <div
+                      className="
+                  flex-1
+                  bg-white
+                  mb-16
+                  overflow-hidden
+                "
+                    >
+                      <div className="md:mt-16">
+                        <a href="https://razorpay.com/?utm_adgroup=brandsearch_core&utm_gclid=Cj0KCQjw8IaGBhCHARIsAGIRRYrtt56ipBzsWEYmePHHooMEOGodS81P-WRpdsezhoUU5HJfCseKjP8aAlM0EALw_wcB&utm_source=google&utm_medium=cpc&utm_campaign=brandsearch&utm_term=razorpay&hsa_src=g&hsa_ad=430853793014&hsa_kw=razorpay&hsa_mt=e&hsa_acc=9786800965&hsa_net=adwords&hsa_ver=3&hsa_grp=27293859910&hsa_tgt=aud-368450393986:kwd-337200051286&hsa_cam=400139470&gclid=Cj0KCQjw8IaGBhCHARIsAGIRRYrtt56ipBzsWEYmePHHooMEOGodS81P-WRpdsezhoUU5HJfCseKjP8aAlM0EALw_wcB">
+                          <img
+                            className="w-72 h-20 md:w-48 md:h-12 m-auto align-baseline "
+                            src={logo3}
+                            alt="img"
+                          />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </Marquee>
+
+                <Marquee
+                  gradient={false}
+                  direction="right"
+                  speed="30"
+                  gradientWidth="0"
+                >
+                  <div className="w-full   md:w-1/3 p-2 flex flex-col flex-grow flex-shrink">
+                    <div
+                      className="
+                  flex-1
+                  bg-white
+                  mb-10 md:mb-8
+                  overflow-hidden
+                "
+                    >
+                      <div className="mt-4">
+                        <a href="https://www.ggtv.co/">
+                          <img
+                            className="w-84 h-48 md:w-44  md:h-28 m-auto align-baseline"
+                            src={logo4}
+                            alt="img"
+                          />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="w-full   md:w-1/3 p-2 flex flex-col flex-grow flex-shrink">
+                    <div
+                      className="
+                  flex-1
+                  bg-white
+                  mb-10 md:mb-8
+                  overflow-hidden
+                "
+                    >
+                      <div className=" ">
+                        <a href="https://www.zostel.com/">
+                          <img
+                            className=" w-40 h-40 m-auto align-baseline "
+                            src={logo6}
+                            alt="img"
+                          />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="w-full   md:w-1/3 p-2 flex flex-col flex-grow flex-shrink">
+                    <div
+                      className="
+                  flex-1
+                  bg-white
+                  mb-10 md:mb-8
+                  overflow-hidden
+                "
+                    >
+                      <div className=" ">
+                        <a href="https://startupgyaan.com/">
+                          <img
+                            className=" w-28 h-28 m-auto align-baseline "
+                            src={logo5}
+                            alt="img"
+                          />
+                          <p className="text-center font-normal text-3xl">
+                            StartupGyaan
+                          </p>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </Marquee>
+              </div>
+            </section>
+          </div>
         </div>
       </div>
     </div>
--- src/Mycomponents/BookmarkComponents/SearchBar.js
+++ src/Mycomponents/BookmarkComponents/SearchBar.js
@@ -12,6 +12,7 @@ function Search() {
   const [tag, setTag] = useState();
   const [flag, setFlag] = useState(false);
   const [isloading, setisloading] = useState(false);
+  const [showtagsection, setshowtagsection] = useState(false)
 
 
   const url = "https://topgun-test1.herokuapp.com/api/bookmarks/?format=json";
@@ -27,10 +28,10 @@ function Search() {
     });
   };
   const getTagsFromApi = async () => {
-    
+
     axios.get(url2).then((res) => {
       setshowalltags(res.data);
-      
+
       // console.log(res.data);
     });
   };
@@ -62,9 +63,9 @@ function Search() {
     var items = [];
     for (var i = 0; i < array.length; i++) {
       items.push(
-        <div className="capitalize inline-block w-auto border-2 rounded-xl text-center bg-gray-200 text-gray-900 mt-2 px-2 text-sm">
+        <span className="capitalize inline-block w-auto border-2 rounded-xl text-center bg-gray-200 text-gray-900 mt-2 mr-2 px-2 text-sm">
           {array[i]}
-        </div>
+        </span>
       );
     }
     return items;
@@ -88,44 +89,54 @@ function Search() {
 
   function cardDisplay(object) {
     return (
-      <div
-        key={object.id}
-        className="break-inside p-4 my-0 mb-4 mx-4 md:mx-1 transition  shadow-md border-2 border-gray-300 rounded-lg bg-gray-50"
-      >
-        <div
-          className=" mt-4 justify-center overflow-hidden transform transition hover:scale-105 hover:mb-2 
-                 break-words p-6 md:p-8 my-1  "
-        >
-          {/* for dummy image */}
-          {object.image.length !== 0 ? (
-            <img src={object.image} alt="" className="object-contain mx-auto" />
-          ) : (
-            <img
-              src="https://via.placeholder.com/150?text=TopGun.vc"
-              alt={object.name}
-              className="object-contain mx-auto"
-            />
-          )}
 
-          <div className="text-center text-lg mt-4">{object.name}</div>
-          <div className="flex flex-col items-center justify-center px-2 py-3">
-            <a href={object.url} target="_blank" rel="noreferrer">
-              <div className="bg-green-500 py-2 px-3 text-white m-2 transition hover:bg-green-600 font-bold">
-                Visit Site
+
+      <div className="break-inside">
+        <a href={object.url} target="_blank" rel="noreferrer">
+          <div
+            className=" mt-0 mb-4 mx-1 justify-center overflow-hidden transform transition hover:scale-105 bg-white
+                 break-words p-6 md:p-0 my-0 border-2 border-gray-300 rounded-md"  key={object.id}
+          >
+            {/* for dummy image */}
+            {object.image.length !== 0 ? (
+              <img src={object.image} alt="" className="object-contain mx-auto w-full" />
+            ) : (
+              <img
+                src="https://via.placeholder.com/150?text=TopGun.vc"
+                alt={object.name}
+                className="object-contain mx-auto mt-2 bg-white"
+              />
+            )}
+
+            <div className="text-center bg-white text-lg mt-0 ">
+              <div className=" p-4 shadow-md">
+                {object.name}
               </div>
-            </a>
+              <div className="flex bg-gray-100  flex-col items-center justify-center px-0 py-0">
 
-            {fun(object.tags)}
+
+                <div className=" pb-2">
+                  {fun(object.tags)}
+                </div>
+
+              </div>
+
+
+
+            </div>
           </div>
-        </div>
+        </a>
       </div>
+
     );
   }
 
   function tagShow(e) {
+    setshowtagsection(false) 
+    
     e.preventDefault();
     setFlag(true);
-
+   
     console.log("Data1 type=", typeof data1);
     if (typeof data1 !== "undefined") {
       const tag_data = data1.filter((obj) => {
@@ -181,9 +192,83 @@ function Search() {
 
   return (
     // search bar
-    <div className="Search my-4 mb-10 overflow-visible  flex flex-col items-center justify-center">
-      <div className=" border-2 border-gray-200 shadow-sm md:shadow-md w-80 md:w-2/3 relative   bg-gray-100 rounded-lg flex">
-        {/* <SearchIcon> */}
+    <div className="Search -mt-1 mb-10 overflow-visible  flex flex-col  items-center justify-center">
+
+      <div className="w-screen">
+        <button
+          className="m-1 ml-4 px-4 mt-4  bg-gray-200 hover:bg-gray-300 rounded-full  font-bold text-xl leading-loose cursor-pointer outline-noneflex float-left items-end"
+          onClick={()=>setshowtagsection(!showtagsection)}
+        >
+        
+        <div>
+          
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
+</svg> 
+          Topics
+          
+          
+        </button>
+        
+
+        <div>
+          <div className=" border-2 mt-2 border-gray-200 shadow-sm md:shadow-md w-80 md:w-4/5   bg-gray-100 rounded-full flex">
+
+            
+            <input
+              className=" placeho w-full rounded-full bg-gray-100 p-2  h-12  placeholder-gray-900"
+              type="text"
+              placeholder="Type and press enter to search..."
+              onChange={exactSearch}
+              value={Search}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  onChangeHandler();
+                }
+              }}
+            />
+            <button
+              onClick={onChangeHandler}
+              className="m-1 mr-5 rounded-lg bg-green-500 px-4 text-gray-100 hover:bg-green-600"
+            >
+              Search
+            </button>
+          </div>
+          
+        </div>
+        {console.log(showtagsection)}
+        {
+          showtagsection?(
+            <div className=" z-0 bg-gray-100 p-5 px-10 m-4 h-48 w-full overflow-scroll">
+            {
+            isloading?<Loading/>:(
+              
+             <div className=" ">
+               {displayalltags}
+             </div>
+            
+      
+             
+            
+            )
+          }
+            </div>
+          ):""
+        }
+        
+
+        
+
+      </div>
+
+
+
+
+
+      {/* <div className=" border-2 mt-2 border-gray-200 shadow-sm md:shadow-md w-80 md:w-2/3   bg-gray-100 rounded-lg flex">
+      Hello
+        
         <input
           className=" placeho w-full rounded-full bg-gray-100 p-2  h-12  placeholder-gray-900"
           type="text"
@@ -202,14 +287,14 @@ function Search() {
         >
           Search
         </button>
-      </div>
-      {
+      </div> */}
+      {/* {
         isloading?"":(
-          <div className="my-3 flex flex-wrap -m-1 w-3/4 md:w-2/3">
+          <div className="my-3  flex flex-wrap -m-1 w-3/4 md:w-2/3">
           <div className="m-1 text-xl block rounded-full px-2 leading-loose outline-none ">
             Topics-
           </div>
-         <div className="">
+         <div className=" ">
            {displayalltags}
          </div>
         
@@ -217,15 +302,15 @@ function Search() {
          
         </div>
         )
-      }
-     
+      } */}
+
 
       {/* content */}
-      
+
       {
         isloading?<Loading/>:(
 
-          <div className="masonry coloumn  before:box-inherit after:box-inherit w-full md:w-4/5 md:px-8">
+          <div className="masonry coloumn overflow-visible before:box-inherit after:box-inherit w-full mt-4   md:px-4">
         
           {
             isloading?<Loading/>:tag ? tag.map((objetc) => cardDisplay(objetc)) : listToDisplay
@@ -235,7 +320,7 @@ function Search() {
 
         )
       }
-      
+
 
     </div>
   );
