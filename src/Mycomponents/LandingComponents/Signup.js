b91ff94368de9092650dfb2873eb346fa2d8c7b6
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-07-09 11:44:36 +0530

    improved login and signup

--- src/Mycomponents/LandingComponents/Signup.js
+++ src/Mycomponents/LandingComponents/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import { DataContext } from "../../ContextApi";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Signup() {
   const setparameters = useContext(DataContext)[1];
@@ -8,9 +9,11 @@ function Signup() {
   const [email, setEmail] = useState("");
   const [password1, setPassword1] = useState("");
   const [password2, setPassword2] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const HandleForm = (event) => {
     event.preventDefault();
+    setLoading(true);
     axios
       .post("https://topgun-test1.herokuapp.com/", {
         username: username,
@@ -32,6 +35,7 @@ function Signup() {
             loading: false,
           },
         ]);
+        setLoading(false);
       })
       .catch((error) => {
         let data = error.response.data;
@@ -47,6 +51,7 @@ function Signup() {
             loading: false,
           },
         ]);
+        setLoading(false);
       });
   };
 
@@ -59,13 +64,13 @@ function Signup() {
            md:pb-16 md:pt-8 mt-4 rounded-md"
           >
             <div>
-              <img
+              {/* <img
                 className="mx-auto h-8 w-auto"
                 src="https://www.act.org/content/dam/act/unsecured/Images/2020/icon-medium-blue-register.png"
                 alt="Workflow"
-              />
-              <h2 className="mt-6 text-center text-2xl  font-medium text-gray-700">
-                Sign Up to Get Exclusive Benefits!
+              /> */}
+              <h2 className="mt-6 text-center text-2xl font-medium text-gray-800">
+                Become a part of TopGun 😎
               </h2>
             </div>
             <form
@@ -80,7 +85,7 @@ function Signup() {
                   <label htmlFor="username" className="sr-only">
                     Username
                   </label>
-                  <div className="text-gray-900 font-bold">Username</div>
+                  <div className="font-normal text-gray-700 ">Username</div>
                   <input
                     id="username"
                     name="username"
@@ -88,7 +93,7 @@ function Signup() {
                     autoComplete="email"
                     required
                     className="p-2 appearance-none relative block w-full md:px-3 md:py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm  mb-3 md:mb-4 bg-gray-50"
-                    placeholder="Enter Username"
+                    placeholder=""
                     value={username}
                     onChange={(event) => setUsername(event.target.value)}
                   />
@@ -97,7 +102,7 @@ function Signup() {
                   <label htmlFor="email" className="sr-only">
                     Email
                   </label>
-                  <div className="text-gray-900 font-bold">Email</div>
+                  <div className="font-normal text-gray-700 ">Email</div>
 
                   <input
                     id="email"
@@ -115,7 +120,7 @@ function Signup() {
                   <label htmlFor="password" className="sr-only">
                     Password
                   </label>
-                  <div className="text-gray-900 font-bold">Password</div>
+                  <div className="font-normal text-gray-700 ">Password</div>
                   <input
                     id="password"
                     name="password"
@@ -132,7 +137,7 @@ function Signup() {
                   <label htmlFor="password" className="sr-only">
                     Password
                   </label>
-                  <div className="text-gray-900 font-bold">
+                  <div className="font-normal text-gray-700 ">
                     Re-Enter-Password
                   </div>
                   <input
@@ -154,10 +159,16 @@ function Signup() {
                   type="submit"
                   className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm  font-medium  rounded-md text-gray-100 bg-blue-500 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300  mt-8"
                 >
-                  SIGN UP
+                  {loading ? "Loading..." : "Sign Up"}
                 </button>
               </div>
             </form>
+            <div className="text-center text-gray-700">
+              Already have an account{" "}
+              <Link to="/login" className="text-blue-500 underline">
+                Login
+              </Link>
+            </div>
           </div>
         </div>
       </div>
